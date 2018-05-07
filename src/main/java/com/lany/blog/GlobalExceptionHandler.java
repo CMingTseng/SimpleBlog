9d1ccbb26d8a1c1283ee84d7d10964a5cbd7f872
Lany <lany192@qq.com>
2018-05-07 22:04:37 +0800

    update

---
+++ src/main/java/com/lany/blog/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.lany.blog;
+
+import com.lany.blog.result.ResponseResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseResult handleException(HttpServletRequest request, Exception ex) {
+        ResponseResult result = new ResponseResult();
+        result.setCode(500);
+        result.setMsg(ex.getMessage());
+        return result;
+    }
+}
